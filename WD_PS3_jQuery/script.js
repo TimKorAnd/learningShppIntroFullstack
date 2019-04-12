a36c1af89bc98a8901c2682d461921386ff8c8e5
TimKorAnd <timkorand@gmail.com>
2019-04-12 06:38:23 +0300

    trying to modules PS_2

--- WD_PS3_jQuery/script.js
+++ WD_PS3_jQuery/script.js
@@ -4,3 +4,48 @@ class SelectElement{
     }
 
 }
+window.onload = ()=>{
+    console.log('onload worked');
+};
+class Kitten {
+    constructor() {
+        this.name = 'Garfield';
+        this.color = 'brown';
+        this.gender = 'male';
+    }
+
+    setName(name) {
+        this.name = name;
+        return this;
+    }
+
+    setColor(color) {
+        this.color = color;
+        return this;
+    }
+
+    setGender(gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    save() {
+        console.log(
+            `saving ${this.name}, the ${this.color} ${this.gender} kitten`
+        );
+
+        // save to database
+
+        return this;
+    }
+}
+
+// use it
+let cat = new Kitten()
+    .setName('Bob')
+    .setColor('black')
+    .setGender('male')
+    .save();
+
+console.log(cat);
+
