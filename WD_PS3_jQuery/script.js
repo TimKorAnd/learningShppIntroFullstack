c41fae86f769f9d803762b85b6da0e1c07d79dfa
TimKorAnd <timkorand@gmail.com>
2019-06-20 02:55:28 +0300

    ps3 ul li

--- WD_PS3_jQuery/script.js
+++ WD_PS3_jQuery/script.js
@@ -1,7 +1,7 @@
 'use strict';
 $(document).ready(function(){
 
-    let sel = new Select('test-select');
+    let sel = new Select('.custom-select');
     //sel.showSelect();
 
 });
@@ -10,7 +10,18 @@ $(document).ready(function(){
 class Select {
 
     constructor(selectClassName) {
+        const $sel = $(selectClassName);
+        this.eventsAttach($sel);
     }
 
+    eventsAttach($sel) {
+        $sel.children('li').each((i, elem) => {
+            $(elem).on('mouseenter', () => {
+                $(elem).css('color', 'blue');
+            }).on('mouseout', () => {
+                $(elem).css('color', 'black');
+            })
+        })
+    }
 
 }
