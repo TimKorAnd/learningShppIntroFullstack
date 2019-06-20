555dc2258c36c8d7b2f09753bb975ac9cd8152db
TimKorAnd <timkorand@gmail.com>
2019-06-20 21:45:42 +0300

    sel work with events

--- WD_PS3_jQuery/script.js
+++ WD_PS3_jQuery/script.js
@@ -15,13 +15,25 @@ class Select {
     }
 
     eventsAttach($sel) {
-        $sel.children('li').each((i, elem) => {
+        /*$sel.children('li').each((i, elem) => {
             $(elem).on('mouseenter', () => {
                 $(elem).css('color', 'blue');
             }).on('mouseout', () => {
                 $(elem).css('color', 'black');
             })
+        })*/
+        $sel.children('li').on('mouseenter', (e) => {
+            $(e.target).closest('li').css('color', 'blue');
+            console.log(`enter ${$(e.target).closest('li')}`);
+        }).on('mouseleave', (e) => {
+            $(e.target).closest('li').css('color', 'black');
+            console.log(`leave ${$(e.target).closest('li')}`);
         })
+
+        $sel.children('li').on('click', (e) => {
+            $(e.target).closest('li').not(':first-child').addClass('option-hide');
+        })
+
     }
 
 }
