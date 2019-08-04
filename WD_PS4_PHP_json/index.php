7fc730718378ee3efd66bf1f1bc92cc1adbb318d
TimKorAnd <timkorand@gmail.com>
2019-08-04 21:13:53 +0300

    delete jquery lib

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Привет, я - скрипт PHP!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
+<body>
+  <?php
+        echo "Привет, я - скрипт PHP!";
+        ?>
+</body>
+</html>
