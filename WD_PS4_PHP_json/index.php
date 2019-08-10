c8bf137c055af6115030125b02ce49cfa29ce935
TimKorAnd <timkorand@gmail.com>
2019-08-10 03:19:36 +0300

    php learning

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='ru'>
 <head>
-  <meta charset="UTF-8">
-  <title>Title</title>
+  <title>Простейший PHP-сценарий</title>
+  <meta charset='utf-8'>
 </head>
 <body>
+<h1>Здравствуйте!</h1>
+<p>
   <?php
-        echo "Привет, я - скрипт PHP!";
-        ?>
-</body>
-</html>
+  // Вычисляем текущую дату в формате "день.месяц год"
+  $dat = date("d.m/y");
+  // Вычисляем текущее время
+  $tm = date("h:i:s");
+  # Выводим их
+  echo "Текущая дата: $dat года<br />\n";
+  echo "Текущее время: $tm<br />\n";
+  # Выводим цифры
+  echo "А вот квадраты и кубы первых 5 натуральных чисел:<br />\n";
+  echo "<ul>\n";
+  for ($i = 1; $i <= 5; $i++) {
+    echo "<li>$i в квадрате = " . ($i * $i);
+    echo ", $i в кубе = " . ($i * $i * $i) . "</li>\n";
+  }
+  ?>
+  </ul>
+</p>
+</body></html>
