54f4e42c56f82fa753a9e80a6109b9bf0bcf9472
TimKorAnd <timkorand@gmail.com>
2019-08-11 09:10:18 +0300

    PHP learning4

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -1,47 +1,7 @@
-<!--<!DOCTYPE html>
-<html lang='ru'>
-<head>
-  <title>Простейший PHP-сценарий</title>
-  <meta charset='utf-8'>
-</head>
-<body>
-<h1>Здравствуйте!</h1>
-<p>
-  <?php
-/*  function  testNameOfFunc(){
-      echo "function ". __FUNCTION__." <br />\n";
-      echo "class ". __CLASS__." <br />\n";
-      echo "os ". PHP_OS." <br />\n";
-  }
-  // Вычисляем текущую дату в формате "день.месяц год"
-  $dat = date("d.m/y");
-  // Вычисляем текущее время
-  $tm = date("h:i:s");
-  # Выводим их
-  echo "Текущая дата: $dat года<br />\n";
-  echo "line ". __LINE__." <br />\n";
- testNameOfFunc();
-  echo "Текущее время: $tm<br />\n";
-  # Выводим цифры
-  echo "А вот квадраты и кубы первых 5 натуральных чисел:<br />\n";
-  echo "Version PHP ". PHP_VERSION." <br />\n";
-  echo "line again ". __LINE__." <br />\n";
-  echo "<ul>\n";
-  for ($i = 1; $i <= 5; $i++) {
-    echo "<li>$i в квадрате = " . ($i * $i);
-    echo ", $i в кубе = " . ($i * $i * $i) . "</li>\n";
-  }
-  */?>
-  </ul>
-</p>
-</body></html>-->
-<?php #l ~cnonb30BaHll!e var_export().
-class SomeClass
-{
-  private $x = 100;
-}
-$a= array(1, array ("Programs hacking programs. Why?", ".д'ApTaHbяH"));
-echo "<pre>"; var_export($a); echo "</pre>";
-$obj= new SomeClass();
-echo "<pre>"; var_export($obj); echo "</pre>";
+
+<?php
+    $index = mt_rand(1,10);
+    $name = "VALUE{$index}";
+    define($name, 777);
+    echo "Constant {$name} has value {${constant($name)}}";
 ?>
