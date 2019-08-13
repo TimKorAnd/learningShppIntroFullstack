0c489c475f641d1f0517f93402cdbe66905c1564
TimKorAnd <timkorand@gmail.com>
2019-08-13 08:03:45 +0300

    php6

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -4,4 +4,8 @@
     $name = "VALUE{$index}";
     define($name, 777);
     echo "Constant {$name} has value {${constant($name)}}";
+
+$a =array( 'a'=>'apple', 'b'=>'banana', 'c'=>array( 'x', 'y', 'z'));
+print_r ($a);
+echo "<pre>"; print_r ($a); echo "</pre>";
 ?>
