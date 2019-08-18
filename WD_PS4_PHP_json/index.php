393506c6b37e95a5bb6351a646abc59615df367c
TimKorAnd <timkorand@gmail.com>
2019-08-18 20:30:19 +0300

    shpp work

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -4,8 +4,22 @@
     $name = "VALUE{$index}";
     define($name, 777);
     echo "Constant {$name} has value {${constant($name)}}";
+    echo "<br>";
 
 $a =array( 'a'=>'apple', 'b'=>'banana', 'c'=>array( 'x', 'y', 'z'));
-print_r ($a);
-echo "<pre>"; print_r ($a); echo "</pre>";
+$b=['one', 'two', 'three'];
+$c=['four', 'five', 'six', 'six with half'];
+$d=['seven', 'eight', 'nine','','trrrr',''];
+$e=['ten'];
+//print_r ($a."<br>");
+//print_r ($b+$c+$d+$e."<br>");
+$all =  $e + $b + $c;
+print_r ($all);
+echo "<br>";
+//$b += $c; $b += $d; $b += $e;
+//$b = $c + $d + $e;
+//print_r ($b);
+echo "<br>";
+
+echo "<pre>"; /*print_r ($a);*/ echo "</pre>";
 ?>
