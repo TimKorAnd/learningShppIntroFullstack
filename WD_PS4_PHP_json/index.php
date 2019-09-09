a75e913b3036c5d7790ea547fed99625d525efb0
TimKorAnd <timkorand@gmail.com>
2019-09-10 01:03:40 +0300

    ps4 1

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -1,81 +1,34 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <title>Выводит все глобальные переменные</title>
-    <meta charset='utf-8'>
-</head>
-<body>
-<?php if (!isset($_REQUEST['doGo'])) {?>
-    <form action="<?=$_SERVER['SCRIPT_NAME']?>" method="get">
-        Логин: <input type="text" name="form[login]" value=""><br />
-        Пароль: <input type="password" name="form[password]" value=""><br />
-        <select name="form[options][]" multiple>
-            <option>First<loption>
-            <option>Second</option>
-            <option>Third</option>
-        </select><br />
-        <input type="submit" name="doGo" value="Нажмите кнопку!">
-    </form>
-<?php } else {
-    if ($_REQUEST['form']['login'] == "root" && $_REQUEST['form']['password'] == "Z10N0101") {
-        echo "Доступ открыт для пользователя {$_REQUEST['form']['login']}";
-        // Команда блокирования рабочей станции (работает в NT-системах)
-        //system("rundll32.exe user32.dll,LockWorkStation");
-    } else {
-        echo "Доступ закрыт!";
-    }
-} ?>
 <?php
+declare(strict_types = 1);
 
-// Вначале счетчик равен нулю.
-$count = 0;
-// Если в Cookies что-то есть, берем счетчик оттуда.
-if (isset($_COOKIE['count'])) $count = $_COOKIE['count'];
-$count++;
-// Записываем в Cookies новое значение счетчика.
-setcookie("count", $count, 0x7FFFFFFF, "/");
-// Выводим счетчик.
-echo $count;
-?>
-
-
-<pre>
-  <?php
-  print_r($GLOBALS);
-  ?>
-  </pre>
-</body>
-</html>
-
-<!--//    $index = mt_rand(1,10);
-//    $name = "VALUE{$index}";
-//    define($name, 777);
-//    echo "Constant {$name} has value {"${constant($name)}"}";
-//
-//$a =array( 'a'=>'apple', 'b'=>'banana', 'c'=>array( 'x', 'y', 'z'));
-//print_r ($a);
-//echo "<pre>"; print_r ($a); echo "</pre>";-->
-<!--
-$arr = [];
-for ($col = 0; $col < 10; $col ++) {
-  for ($row = 0; $row < 10; $row++) {
-    $arr['col' . $col]['row' . $row] = ($col * 10) + $row;
-  }
+function generateNextDigitInRange(int $from = 0, int $to = 0) {
+    for ($i = $from; $i <= $to; $i++)
+        yield $i;
 }
-  print_r($arr);
-echo '<br>';
 
-  for (reset($arr); $key = key($arr);  next($arr))
-  {
-    $val = current($arr);
-    for (reset($val); key($val);  next($val))
-    {
-      $lastVal = current($val);
-      echo $lastVal.' ';
+function ascendLimits(int &$from, int &$to): void {
+    if ($to < $from) [$from, $to] = [$to, $from];
+    echo "to is {$to}; from is {$from} <br>";
+}
 
+function rangeSum (int $from = 0, int $to = 0): int {
+    ascendLimits($from, $to);
+    $sum = 0;
+    echo "to is {$to}; from is {$from} <br>";
+    foreach (generateNextDigitInRange($from, $to) as $currentDigit) {
+      $sum += $currentDigit;
     }
-    echo '<br>';
+    return $sum;
+}
+echo "start<br>";
+try {
+    $result = rangeSum(1000,-1000);
+    echo "sum is {$result} asasas";
+    echo "dfsdfasdf </br>";
+    echo "stop </br>";
+} catch (\Exception $e){
+  var_dump($e);
+}
 
-  }
-?>-->
 
+?>
