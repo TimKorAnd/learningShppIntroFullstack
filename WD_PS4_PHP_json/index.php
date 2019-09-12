8223114bc0d7818d62c2cb1419b409c152205c45
TimKorAnd <timkorand@gmail.com>
2019-09-13 00:21:59 +0300

    ps4 still start

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -1,32 +1,38 @@
 <?php
 declare(strict_types = 1);
 
-function generateNextDigitInRange(int $from = 0, int $to = 0) {
-    for ($i = $from; $i <= $to; $i++)
+function generateNextDigitInRangeByFilter(int $from = 0, int $to = 0, $cbFiltering ) {
+    for ($i = $from; $i <= $to; $i++) {
+        if ($cbFiltering($i))
         yield $i;
+    }
 }
 
-function ascendLimits(int &$from, int &$to): void {
+function sortAscendingLimits(int &$from, int &$to): void {
     if ($to < $from) [$from, $to] = [$to, $from];
     //echo "to is {$to}; from is {$from} <br>";
 }
 
-function rangeSum (int $from = 0, int $to = 0): int {
-    ascendLimits($from, $to);
+function rangeSum (int $from = 0, int $to = 0, $cbFiltering): int {
+    sortAscendingLimits($from, $to);
     $sum = 0;
     //echo "to is {$to}; from is {$from} <br>";
-    foreach (generateNextDigitInRange($from, $to) as $currentDigit) {
+    foreach (generateNextDigitInRangeByFilter($from, $to, $cbFiltering) as $currentDigit) {
       $sum += $currentDigit;
     }
     return $sum;
 }
 echo "start<br>";
+$from = 0;
+$to = -4;
 try {
-    $result = rangeSum(1000,-1000);
-    echo "sum is {$result} asasas";
-    echo "dfsdfasdf </br>";
+    $result = rangeSum($from, $to,  function(){return true;});
+    echo "Task1. Sum from  {$from} to {$to} is {$result}<br>\n";
+    $result = rangeSum($from, $to,  function($e){return $e % 2 == 0;});
+    echo "Task2. Sum from  {$from} to {$to} is {$result})}<br>\n";
+
     echo "stop </br>";
-} catch (\Exception $e){
+} catch (Exception $e){
   var_dump($e);
 }
 
