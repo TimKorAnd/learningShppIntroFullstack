06387b7a41bb276c4756c9be01f8943ceecf0862
TimKorAnd <timkorand@gmail.com>
2019-09-15 21:32:46 +0300

    ps4 still start

--- WD_PS4_PHP_json/controller.php
+++ WD_PS4_PHP_json/controller.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types = 1);
+require_once "dumper.php";
 
 function generateNextDigitInRangeByFilter(int $from = 0, int $to = 0, $cbFiltering ) {
     for ($i = $from; $i <= $to; $i++) {
@@ -22,31 +23,46 @@ function rangeSum (int $from = 0, int $to = 0, $cbFiltering): int {
     }
     return $sum;
 }
-//echo "start<br>";
+//echo "controller start <br>";
 $from = 0;
 $to = 0;
-$result = 0;
+$result = "";
+
 define("PATTERN_237", "/[237]$/");
+/*array of different tasks functions*/
+$taskFunctions = [
+    ['task1'] => function () {
+        return true;
+    },
+    ['task2'] => function ($e) {
+        return preg_match(PATTERN_237, strval($e));
+    }];
+
 if (!empty($_REQUEST['doAdd'])) {
 //if (true) {
-//    var_dump($_REQUEST);
-//    var_dump($_POST['task1-from']);
-    $from = intval($_REQUEST['task1-from']);
-    $to = intval($_REQUEST['task1-to']);
-    try {
-        $result = rangeSum($from, $to, function () {
-            return true;
-        });
-        echo "Task1. Sum from  {$from} to {$to} is {$result}<br>\n";
-        $result = rangeSum($from, $to, function ($e) {
-            return preg_match(PATTERN_237, strval($e));
-        });
-        echo "Task2. Sum from  {$from} to {$to} is {$result})<br>\n";
+    dumper($_REQUEST);
 
-        echo "stop </br>";
+    $from = intval($_REQUEST['task1-2']['from']);
+    $to = intval($_REQUEST['task1-2']['to']);
+    try {
+        foreach ($_REQUEST['task1-2']['taskStatus'] as $taskStatus) {
+            var_dump($taskStatus);
+            if ($taskStatus == '1')
+                $result .= rangeSum($from, $to, $taskFunctions[$taskStatus]);
+            //echo "Task1. Sum from  {$from} to {$to} is {$result}<br>\n";
+
+
+            /*$result = rangeSum($from, $to, function ($e) {
+                return preg_match(PATTERN_237, strval($e));
+            });
+            echo "Task2. Sum from  {$from} to {$to} is {$result})<br>\n";*/
+
+
+            echo "stop </br>";
+        }
     } catch (Exception $e) {
         var_dump($e);
     }
 }
-include "view.php";
+include "./view.php";
 ?>
--- WD_PS4_PHP_json/view.php
+++ WD_PS4_PHP_json/view.php
@@ -3,13 +3,25 @@
 <body>
 <h1>Task1.</h1>
 <form action="./controller.php" method="post">
-  <label for="task1-from">Enter from:</label>
-  <input type="number" id="task1-from" name="task1-from"><br />
-  <label for="task1-to">Enter to:</label>
-  <input type="number" id="task1-to" name="task1-to"><br />
-  <input type="submit" name="doAdd" value="Sum">
+
+    <label for="task1-from">Enter from:</label>
+    <input type="number" id="task1-from" name="task1-2[from]">
+    <label for="task1-to"> Enter to:</label>
+    <input type="number" id="task1-to" name="task1-2[to]"><br />
+    <!--for choose algorithm of calculating between task1 or task2-->
+    <label for="task1-calculate"> Calculate all digits in range</label>
+    <input type="hidden" name="task1-2[taskStatus][1]" value="0">
+    <input type="checkbox" id= "task1-calculate" name="task1-2[taskStatus][task1]" value="1">
+    <label for="task2-calculate"> Calculate only digits which ends of 2,3,7 in range</label>
+    <input type="hidden" name="task1-2[taskStatus][2]" value="0">
+    <input type="checkbox" id= "task2-calculate" name="task1-2[taskStatus][task2]" value="1"><br />
+
+    <input type="submit" name="doAdd" value="Sum">
+
 </form>
 <h2>Result is</h2>
-<?php echo $result; ?>
+<?php
+    echo $result; ?>
+
 
 </body></html>
