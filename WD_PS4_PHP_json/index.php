748822b4ac42960dccd1f11de39030e46c1dbcf7
TimKorAnd <timkorand@gmail.com>
2019-09-20 16:34:27 +0300

    ps4 task3 start

--- WD_PS4_PHP_json/index.php
+++ WD_PS4_PHP_json/index.php
@@ -1,82 +1,15 @@
 <?php
-/*controller*/
-declare(strict_types = 1);
-require_once "dumper.php";
+declare(strict_types=1);
+$results = [];
 
-function generateNextDigitInRangeByFilter(int $from = 0, int $to = 0, $cbFiltering ) {
-    for ($i = $from; $i <= $to; $i++) {
-        if ($cbFiltering($i))
-        yield $i;
-    }
-}
-
-function sortAscendingLimits(int &$from, int &$to): void {
-    if ($to < $from) [$from, $to] = [$to, $from];
-    //echo "to is {$to}; from is {$from} <br>";
-}
-
-function rangeSum (int $from = 0, int $to = 0, $cbFiltering): int {
-    sortAscendingLimits($from, $to);
-    $sum = 0;
-    //echo "to is {$to}; from is {$from} <br>";
-    foreach (generateNextDigitInRangeByFilter($from, $to, $cbFiltering) as $currentDigit) {
-      $sum += $currentDigit;
-    }
-    return $sum;
-}
-
-/**
- * @param string $from
- * @param string $to
- * @param array $result
- */
-function makeCalculation(int $from, int $to, array &$result): void
-{
-    //dumper($_REQUEST);
-    //global $result;
-    try {
-        $from = intval($_REQUEST['task1-2']['from']);
-        $to = intval($_REQUEST['task1-2']['to']);
-        foreach ($_REQUEST['task1-2']['taskStatus'] as $taskStatus => $v) {
-            /*dumper($taskStatus);
-            echo $v;*/
-            if ($v === '0') {
-                $result[$taskStatus] = " not calculate - unchecked";
-            } else {
-                $result[$taskStatus] = " " . rangeSum($from, $to, $taskStatus);
-                //echo $result[$taskStatus];
-            }
-        }
-    } catch (Exception $e) {
-        var_dump($e);
-    }
-}
-/*tasks functions for generators*/
-function task1 () {
-    return true;
-};
-function task2($e) {
-    return preg_match(PATTERN_237, strval($e));
-};
-//echo "controller start <br>";
-
-$result = [];
-//$result['task1'] = $result['task2'] = 'check some box, enter values, and press Sum, please ';
-define("PATTERN_237", "/[237]$/");
-
+/*variables initialize*/
+$results['task1'] = $results['task2'] = 'check some box, enter values, and press Sum, please ';
 
 if (!empty($_REQUEST['doAdd'])) {
-    $from = ($_REQUEST['task1-2']['from'] === '') ? 0 : intval($_REQUEST['task1-2']['from']);
-    $to = ($_REQUEST['task1-2']['to'] === '') ? 0 : intval($_REQUEST['task1-2']['to']);
-    $isTask1 = !empty($_REQUEST['task1-2']['taskStatus']['task1']);
-    $isTask2 = !empty($_REQUEST['task1-2']['taskStatus']['task2']);
-    if (!$isTask1 && !$isTask2 ) {
-        $result['task1'] = $result['task2'] = 'check some box, please ';
-    } else
-    makeCalculation($from, $to, $result);
+    require_once('private/task12.php');
 
 } else {
-    $result['task1'] = $result['task2'] = 'check some box, enter values, and press Sum, please ';
+
 }
 include "./view.php";
 ?>
--- WD_PS4_PHP_json/view.php
+++ WD_PS4_PHP_json/view.php
@@ -1,11 +1,12 @@
 <!-- MVC. Warmup view -->
 <html><head><title> WD_PS4 Warmup </title></head>
 <body>
-<h1>Task1 & Task2.</h1>
-<form action="index.php" method="post">
-
+<!--<h1>Task1 & Task2.</h1>-->
+<form id="task1-2-calculate" action="index.php" method="post">
+<fieldset>
+  <legend>Task1 & Task2</legend>
     <label for="task1-from">Enter from:</label>
-    <input type="number" id="task1-from" name="task1-2[from]">
+    <input type="number" id="task1-from" name="task1-2[from]" value="">
     <label for="task1-to"> Enter to:</label>
     <input type="number" id="task1-to" name="task1-2[to]"><br />
     <!--for choose algorithm of calculating between task1 or task2-->
@@ -15,18 +16,23 @@
     <input type="checkbox" id= "task1-calculate" name="task1-2[taskStatus][task1]" value="1">
     <?php
           //echo "sum all digits from {$from} to {$to} is {$result['task1']}";
-          echo $result['task1'];
+          echo $results['task1'];
        ?>
   </div>
   <div>
     <label for="task2-calculate"> Calculate only digits which ends of 2,3,7 in range</label>
     <input type="hidden" name="task1-2[taskStatus][task2]" value="0">
     <input type="checkbox" id= "task2-calculate" name="task1-2[taskStatus][task2]" value="1">
-      <?php echo $result['task2'] ?>
+      <?php echo $results['task2'] ?>
   </div>
 
-    <input type="submit" name="doAdd" value="Sum">
-
+    <input id="task12__sbmt-btn" type="submit" name="doAdd" value="Sum">
+</fieldset>
 </form>
+<!--<h1>Task1 & Task2.</h1>-->
+<form id="task3-fileUpload" action="index.php" method="post">
+  <fieldset>
+    <legend>Task3. Files upload</legend>
 
+  </fieldset>
 </body></html>
