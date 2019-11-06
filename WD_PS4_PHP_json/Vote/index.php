2398bebdc12b92a6c8189122736b4ee7427779d4
TimKorAnd <timkorand@gmail.com>
2019-11-06 07:14:30 +0200

    ps4 vote layout

--- WD_PS4_PHP_json/Vote/index.php
+++ WD_PS4_PHP_json/Vote/index.php
@@ -1,1 +1,32 @@
+
 <?php
+    $voteSetName = 'voteSet'; //name of voted
+    $candidateList = ['candidate1', 'candidate2', 'candidate3', 'four']; //TODO read from file
+
+    if ( isset( $_POST[$voteSetName.'__sbmt-btn'] )  ) {
+        $voted = @$_POST[$voteSetName];
+        switch ($voted) {
+            case null :
+                echo 'choose any candidate and vote';
+                include_once('./view.php');
+                break;
+            case '0':
+                echo 'Первый';
+                break;
+            case '1' :
+                echo 'второй';
+                break;
+            case '2' :
+                echo 'тертий';
+                break;
+            case '3' :
+                echo 'четвертый';
+                break;
+        }
+    }
+    else {
+//        echo 'you have to vote';
+        include_once('./view.php');
+    }
+    echo '<br/><a href="index.php">vote again</a>';
+?>
--- WD_PS4_PHP_json/Vote/view.php
+++ WD_PS4_PHP_json/Vote/view.php
@@ -8,16 +8,21 @@
 <!--<h1>Task1 & Task2.</h1>-->
 
 <!--<h1>Task8. Character count</h1>-->
-<form id="vote__list" action="index.php" method="post">
+<form id="voteList" action="index.php" method="post">
   <fieldset>
     <legend>Vote for just one</legend>
     <?php
-    echo "<label for=\"$labelFor\"> $labelText </label>"
-    <input type="radio" <?php ?>sid="task8__chars" name="task8__chars[]"><?php
-        echo @$_SESSION['task8__chars'][0];
-        ?>
-  ?>
-    <input type="submit" id="task8__sbmt-btn" name="task8__sbmt-btn" value="char calc">
+        $count = 0;
+        foreach ( $candidateList as $candidate ) {
+            echo '<label for='.$candidate.'>'.$candidate.' </label>';
+            echo '<input type="radio" id="'.$candidate.$count.'" name="'.$voteSetName.'"
+             value="'.$count++.'">';
+        }
+    ?>
+
+      <?php
+          echo '<input type="submit" id="'.$voteSetName.'__sbmt-btn" name="'.$voteSetName.'__sbmt-btn" value="vote finally">';
+      ?>
 
 
   </fieldset>
