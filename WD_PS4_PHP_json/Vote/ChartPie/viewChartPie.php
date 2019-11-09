1d34c7a5d5a4eb006fba3268eca39cac7ce567ff
TimKorAnd <timkorand@gmail.com>
2019-11-09 08:39:37 +0200

    ps4 vote chartPie structure start

---
+++ WD_PS4_PHP_json/Vote/ChartPie/viewChartPie.php
@@ -0,0 +1,4 @@
+<?php
+    require_once('./index.php');
+    echo "there will pie chart display for ";
+    echo $voted;
--- WD_PS4_PHP_json/Vote/index.php
+++ WD_PS4_PHP_json/Vote/index.php
@@ -1,33 +1,14 @@
 
 <?php
     $config = require_once 'config.php';
-    $voteSetName = $config['voting']['voteSetName']; //name of voted
+    $voteSetName = $config['voting']['voteSetName']; //name of voteSet
     $candidateList =  $config['voting']['candidateList']; //TODO read from file
 
     if ( isset( $_POST[$voteSetName.'__sbmt-btn'] )  ) {
         $voted = @$_POST[$voteSetName];
-        switch ($voted) {
-            case null :
-                echo 'choose any candidate and vote';
-                include_once('./view.php');
-                break;
-            case '0':
-                echo 'Первый';
-                break;
-            case '1' :
-                echo 'второй';
-                break;
-            case '2' :
-                echo 'тертий';
-                break;
-            case '3' :
-                echo 'четвертый';
-                break;
-        }
-    }
-    else {
-//        echo 'you have to vote';
+        include_once('./ChartPie/viewChartPie.php');
+    } else {
         include_once('./view.php');
     }
-    echo '<br/><a href="index.php">vote again</a>';
+    //echo '<br/><a href="index.php">vote again</a>';
 ?>
